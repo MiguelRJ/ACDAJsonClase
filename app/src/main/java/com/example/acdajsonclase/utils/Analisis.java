acdca536383909856c4346b7e4ff59923f694dfe
MiguelRJ <miguel.rj96@gmail.com>
2018-01-23 11:14:46 +0100

    Analisis, no acabado

---
+++ app/src/main/java/com/example/acdajsonclase/utils/Analisis.java
@@ -0,0 +1,28 @@
+package com.example.acdajsonclase.utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by usuario on 23/01/18.
+ */
+
+public class Analisis {
+
+    public static String analizarPrimitiva(JSONObject texto) throws JSONException {
+        JSONArray jsonContenido;
+        String tipo;
+        StringBuilder cadena = new StringBuilder();
+        tipo = texto.getString("info");
+        jsonContenido = new JSONArray(texto.getString("sorteo"));
+        cadena.append("Sorteos de la Primitiva:" + "\n");
+
+        for (int i = 0; i < jsonContenido.length(); i++) {
+
+        }
+
+        return cadena.toString();
+    }
+
+}
