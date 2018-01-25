ffcfebba30a76f55f3d139a006ca402be062875c
MiguelRJ <miguel.rj96@gmail.com>
2018-01-25 14:41:49 +0100

    contactos activity

--- app/src/main/java/com/example/acdajsonclase/utils/Analisis.java
+++ app/src/main/java/com/example/acdajsonclase/utils/Analisis.java
@@ -1,8 +1,13 @@
 package com.example.acdajsonclase.utils;
 
+import com.example.acdajsonclase.ui.contactos.Contacto;
+import com.example.acdajsonclase.ui.contactos.Telefono;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by usuario on 23/01/18.
@@ -15,15 +20,38 @@ public class Analisis {
         String tipo;
         JSONObject item;
         StringBuilder cadena = new StringBuilder();
+
         tipo = texto.getString("info");
         jsonContenido = new JSONArray(texto.getString("sorteo"));
         cadena.append("Sorteos de la Primitiva:" + "\n");
 
         for (int i = 0; i < jsonContenido.length(); i++) {
             item = jsonContenido.getJSONObject(i);
+            cadena.append(tipo + ":" + item.getString("fecha") + "\n");
+            cadena.append(
+                    item.getInt("numero1") + "," +
+                            item.getInt("numero2") + "," +
+                            item.getInt("numero3") + "," +
+                            item.getInt("numero4") + "," +
+                            item.getInt("numero5") + "," +
+                            item.getInt("numero6") + "\n"
+            );
+            cadena.append("Complementario: "+item.getInt("complementario")+", Reintegro: "+item.getInt("reintegro")+"\n");
         }
-
         return cadena.toString();
+    }
+
+    public static ArrayList<Contacto> analizarContactos(JSONObject respuesta) throws JSONException {
+        JSONArray jAcontactos;
+        JSONObject jOcontacto, jOtelefono;
+        Contacto contacto;
+        Telefono telefono;
+        ArrayList<Contacto> personas = new ArrayList<>();
+        // añadir contactos (en JSON) a personas
+
+
+
+        return personas;
     }
 
 }
